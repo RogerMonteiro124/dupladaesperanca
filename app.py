dd61de74863560bee7ef0b3ac7c7f9723c253f9e
Roger Monteiro <roger-15monteiro@hotmail.com>
2024-03-18 21:08:16 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,39 @@
+from flask import Flask, request, render_template, redirect, url_for, send_file
+from threading import Thread
+import csv
+from datetime import datetime
 
-from flask import Flask
+now = datetime.today().strftime('%d-%m-%Y')
+
 app = Flask(__name__)
 
+
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+  return render_template("index.html")
+
+
+@app.route('/get_data', methods=['POST'])
+def get_data():
+  nome = request.form.get('nome')
+  telefone = request.form.get('telefone')
+  pedido = request.form.get('mensagem')
+  phone = request.form.get('phone')
+  text = request.form.get('text')
+
+  with open('dados.csv', 'a', newline='') as arquivo_csv:
+    writer = csv.writer(arquivo_csv)
+    dados = [
+      nome, telefone, pedido
+    ]
+    writer.writerow(dados)
+  
+  return redirect("https://api.whatsapp.com/send?phone=" + str(phone) +
+                  "&text=" + str(text) + "&mensagem=" + str(pedido))
+
+
+@app.route('/live')
+def home():
+  return 'I am Alive in ' + str(now)
+
+
