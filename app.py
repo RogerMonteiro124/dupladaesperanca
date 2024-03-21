b272978d2320da2f88f5d6647a92f513c2722be2
Roger Monteiro <roger-15monteiro@hotmail.com>
2024-03-20 21:59:13 -0400

    add telegram support

--- app.py
+++ app.py
@@ -4,6 +4,13 @@ import csv
 from datetime import datetime
 
 now = datetime.today().strftime('%d-%m-%Y')
+bot = telegram.Bot(token=os.environ['TOKEN'])
+
+def send(text):
+      print("sendou")
+      #telegram_send.send(messages=[text], parse_mode= 'Markdown')
+      bot.sendMessage(chat_id=-4194652894, text=text, parse_mode= 'Markdown')
+      #telegram_send.send(messages=[text], parse_mode= 'Markdown')
 
 app = Flask(__name__)
 
@@ -21,12 +28,24 @@ def get_data():
   phone = request.form.get('phone')
   text = request.form.get('text')
 
-  with open('dados.csv', 'a', newline='') as arquivo_csv:
-    writer = csv.writer(arquivo_csv)
-    dados = [
-      nome, telefone, pedido
-    ]
-    writer.writerow(dados)
+  msg = """
+## Novo contato
+:bulb: Alguém precisa de Esperença :bulb: :pray:
+
+:clipboard: **Nome:"""+str(nome)+"""**  
+:iphone: **Telefone:"""+str(telefone)+"""**  
+:pray: **Pedido de Oração:"""+str(pedido)+"""** 
+"""
+  
+  #send telegram message
+  send(msg)
+
+  # with open('dados.csv', 'a', newline='') as arquivo_csv:
+  #   writer = csv.writer(arquivo_csv)
+  #   dados = [
+  #     nome, telefone, pedido
+  #   ]
+  #   writer.writerow(dados)
   
   return redirect("https://api.whatsapp.com/send?phone=" + str(phone) +
                   "&text=" + str(text) + "&mensagem=" + str(pedido))
